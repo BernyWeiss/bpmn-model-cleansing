b87417753afb789d26956480ae4aae146f4abfcb
BernyWeiss <38976809+BernyWeiss@users.noreply.github.com>
2026-06-18 12:30:54 +0200

    added script to compare english_models text file for reproduction

---
+++ mcp4cm/temporary/compare_english_models_lists.py
@@ -0,0 +1,19 @@
+
+def load_list_as_set(path: str) -> set:
+    with open(path, 'r') as f:
+        return set(f.read().splitlines())
+
+
+def print_set_differences(original_set, my_set) -> None:
+    print("In my set, but not original")
+    print(my_set-original_set)
+
+    print("In original set, but not mine")
+    print(original_set-my_set)
+
+
+original = load_list_as_set("../../data/reproduction/english_models_original.txt")
+mine = load_list_as_set("../../data/reproduction/english_models_generated.txt")
+
+print_set_differences(original, mine)
+

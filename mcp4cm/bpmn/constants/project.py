d40630db49b93549a9e78f7b4a8756364c1a6fbe
BernyWeiss <38976809+BernyWeiss@users.noreply.github.com>
2026-07-08 09:41:50 +0200

    added constants for dataset paths; improved naming of bpmn dataset names

---
+++ mcp4cm/bpmn/constants/project.py
@@ -0,0 +1,7 @@
+from typing import Final
+
+
+BASE_BPMN_DATASETS_PATH: Final[str] = 'data/bpmnmodelset'
+
+SAM_MODELS_PATH: Final[str] = 'sap_sam_2022/models'
+BPMAI_MODELS_PATH: Final[str] = 'bpmai/models'
